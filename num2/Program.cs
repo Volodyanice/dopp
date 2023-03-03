89528b0fb743c97df6820280a7f3a8a4f30eb4ab
Volodya <skochilovvladimir@yandex.ru>
2023-03-04 02:45:12 +0700

    number2

---
+++ num2/Program.cs
@@ -0,0 +1,9 @@
+﻿int quarter = Convert.ToInt32(Console.ReadLine());
+if (quarter == 1)
+    System.Console.WriteLine("x>0; y>0");
+if (quarter == 2)
+    System.Console.WriteLine("x<0; y>0");
+if (quarter == 3)
+    System.Console.WriteLine("x<0; y<0");
+if (quarter == 4)
+    System.Console.WriteLine("x>0; y<0");
